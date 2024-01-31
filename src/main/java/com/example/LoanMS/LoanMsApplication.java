e8b9a20cbaf0de013452fa21ec8561f77fb6496e
yash2797 <yashvanth02071997@gmail.com>
2024-01-31 10:55:07 -0600

    Backend

--- src/main/java/com/example/LoanMS/LoanMsApplication.java
+++ src/main/java/com/example/LoanMS/LoanMsApplication.java
@@ -1,13 +1,26 @@
 package com.example.LoanMS;
 
+import com.example.LoanMS.Storage.StorageProperties;
+import com.example.LoanMS.Storage.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class LoanMsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LoanMsApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(LoanMsApplication.class, args);
+  }
 
+  @Bean
+  CommandLineRunner init(StorageService storageService) {
+    return (args) -> {
+      storageService.deleteAll();
+      storageService.init();
+    };
+  }
 }
